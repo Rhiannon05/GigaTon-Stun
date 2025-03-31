26ce9765b23633310d2164d8ec003c4cdf591416
Rhiannon05 <rdombrowskas@gmail.com>
2025-03-31 14:29:19 -0400

    MainMenuFunctioning
    Got Main menu established and functioning

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
