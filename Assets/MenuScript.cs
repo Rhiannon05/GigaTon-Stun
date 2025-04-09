c9b14940bd3ad69429ed6fe509f6e053f5e82cbe
Rhiannon05 <rdombrowskas@gmail.com>
2025-04-08 21:32:43 -0400

    MenusCompleted

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -6,9 +6,49 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
 
-    public void Game()
+    public void LevelSelect()
     {
         SceneManager.LoadScene("LevelSelect");
+    }
+    public void Antartica()
+    {
+        SceneManager.LoadScene("Antartica");
+    }
+    public void Bridge()
+    {
+        SceneManager.LoadScene("Bridge");
+    }
+    public void Church()
+    {
+        SceneManager.LoadScene("Church");
+    }
+    public void Fields()
+    {
+        SceneManager.LoadScene("Fields");
+    }
+    public void Moon()
+    {
+        SceneManager.LoadScene("Moon");
+    }
+    public void Outback()
+    {
+        SceneManager.LoadScene("Outback");
+    }
+    public void Steakhouse()
+    {
+        SceneManager.LoadScene("Steakhouse");
+    }
+    public void Stadium()
+    {
+        SceneManager.LoadScene("Stadium");
+    }
+    public void BoxingRing()
+    {
+        SceneManager.LoadScene("BoxingRing");
+    }
+    public void Ruins()
+    {
+        SceneManager.LoadScene("Ruins");
     }
     public void Credits()
     {
